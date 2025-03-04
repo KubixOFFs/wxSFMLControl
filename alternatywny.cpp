77834383b345550d24b47fe80cfeeae9d0903b32
jakubogorek <166607372+KubixOFFs@users.noreply.github.com>
2025-03-04 18:29:05 +0100

    Update alternatywny.cpp

--- alternatywny.cpp
+++ alternatywny.cpp
@@ -1,36 +1,50 @@
+#include <wx/wx.h>
+#include <wx/timer.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
 class SFMLCanvas : public wxControl {
 private:
     std::unique_ptr<sf::RenderWindow> renderWindow;
     wxTimer timer;
+    sf::CircleShape circle;
 
 public:
     SFMLCanvas(wxWindow* parent, wxWindowID id)
-        : wxControl(parent, id, wxDefaultPosition, wxDefaultSize, wxWANTS_CHARS), timer(this) {
-        
-        // Pobranie uchwytu okna (SFML potrzebuje HWND na Windowsie)
+        : wxControl(parent, id, wxDefaultPosition, wxSize(400, 300), wxWANTS_CHARS),
+          timer(this), circle(50.0f) {  // Tworzymy zielone koło
+
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(100, 100);
+
+        // Pobranie uchwytu okna i stworzenie SFML RenderWindow
         sf::WindowHandle handle = GetHandle();
-        
-        // Tworzenie SFML RenderWindow
         renderWindow = std::make_unique<sf::RenderWindow>(handle);
 
-        // Uruchomienie timera do odświeżania renderowania
-        timer.Bind(wxEVT_TIMER, &SFMLCanvas::OnTimer, this);
-        timer.Start(16);  // ~60 FPS
+        // Wiązanie eventów za pomocą Bind()
+        Bind(wxEVT_TIMER, &SFMLCanvas::OnTimer, this);
+        Bind(wxEVT_SIZE, &SFMLCanvas::OnSize, this);
+        Bind(wxEVT_ERASE_BACKGROUND, &SFMLCanvas::OnEraseBackground, this);
+
+        // Timer do odświeżania renderowania
+        timer.Start(16);  // Około 60 FPS
     }
 
     void OnTimer(wxTimerEvent&) {
-        if (renderWindow) {
-            renderWindow->clear(sf::Color::Black);
-            // Rysowanie SFML
-            renderWindow->display();
-        }
+        if (!renderWindow) return;
+
+        ProcessEvents();
+        renderWindow->clear(sf::Color::Black);  // Tło czarne
+        renderWindow->draw(circle);  // Rysowanie koła
+        renderWindow->display();
     }
 
-    void OnPaint(wxPaintEvent&) {
-        wxPaintDC dc(this);
-        if (renderWindow) {
-            renderWindow->setActive(true);  // Upewnij się, że SFML ma aktywny kontekst
-            renderWindow->display();
+    void ProcessEvents() {
+        sf::Event event;
+        while (renderWindow->pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                renderWindow->close();
+            }
         }
     }
 
@@ -43,23 +57,16 @@ public:
     }
 
     void OnEraseBackground(wxEraseEvent&) {}  // Zapobiega migotaniu
-
-    wxDECLARE_EVENT_TABLE();
 };
 
-wxBEGIN_EVENT_TABLE(SFMLCanvas, wxControl)
-    EVT_PAINT(SFMLCanvas::OnPaint)
-    EVT_SIZE(SFMLCanvas::OnSize)
-    EVT_ERASE_BACKGROUND(SFMLCanvas::OnEraseBackground)
-wxEND_EVENT_TABLE()
-
-
-
-void ProcessEvents() {
-    sf::Event event;
-    while (renderWindow->pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            renderWindow->close();
-        }
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit() {
+        wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "SFML w wxWidgets", wxDefaultPosition, wxSize(800, 600));
+        new SFMLCanvas(frame, wxID_ANY);
+        frame->Show();
+        return true;
     }
-}
+};
+
+wxIMPLEMENT_APP(MyApp);
